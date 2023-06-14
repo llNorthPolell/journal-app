c2dcab17f6a7e9f3dd9249e9620174c067393a3f
Marco Ng <marcong0131@gmail.com>
2023-06-14 13:50:20 -0400

    Added FlatRecord entity

--- server/journal-app-server/common/src/main/java/com/northpole/common/entity/JournalEntry.java
+++ server/journal-app-server/common/src/main/java/com/northpole/common/entity/JournalEntry.java
@@ -7,17 +7,21 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+@Document("journalEntries")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class JournalEntry {
 
+    @Id
     private UUID entryId;
 
     @NotNull
